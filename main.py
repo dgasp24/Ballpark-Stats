4a2a9edbcee00b128c4d1a142844ee808f6337e9
dgasp24 <dommygabby7@gmail.com>
2026-07-07 21:49:44 -0400

    added selection for either homeruns or batting average

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 from apiLoader import getPlayerID, getTeamID, getPlayerStats
 from plot import generatePlot
-season = "1990"
 
 teams = {}
 player = []
-batting_avg = []
-
-playerData = getPlayerID(season)
-teamData = getTeamID()
-
-for team in teamData['teams']:
-    if team['sport']['name'] == "Major League Baseball":
-        teams[team['teamName'].lower()] = team['id']
-    else:
-        continue
+hitting_stat = []
+isInt = True
 
 while True:
     userTeam = input("Enter team: ")
+    year = int(input("Enter year: "))
+    statChoice = int(input("1. Batting Average\n2. Homeruns\n"))
+    playerData = getPlayerID(year)
+    teamData = getTeamID()
+
+    for team in teamData['teams']:
+        if team['sport']['name'] == "Major League Baseball":
+            teams[team['teamName'].lower()] = team['id']
+        else:
+            continue
     try:
         for players in playerData['people']:
-                if players['currentTeam']['id'] == teams[userTeam.lower()] and (players['primaryPosition']['type'] == "Outfielder" or players['primaryPosition']['type'] == "Infielder"  or players['primaryPosition']['type'] == "Hitter"):
-                    playerStats = getPlayerStats(players['id'], season)['stats'][0]['splits'][0]['stat']
-                    stats = { "avg": playerStats['avg'], 'hr': playerStats['homeRuns']
-                    }
-                    print(f"ID: {players['id']}, {players['firstName']} {players['lastName']}")
+                if (players['currentTeam']['id'] == teams[userTeam.lower()] and
+                        (players['primaryPosition']['type'] == "Outfielder" or
+                         players['primaryPosition']['type'] == "Infielder"  or
+                         players['primaryPosition']['type'] == "Hitter")):
+
+                    playerStats = getPlayerStats(players['id'], year)['stats'][0]['splits'][0]['stat']
+                    stats = { "avg": playerStats['avg'], 'hr': playerStats['homeRuns']}
+                    print(f"{players['firstName']} {players['lastName']}")
                     print(f"Batting Average: {stats['avg']}, Home Runs: {stats['hr']}")
                     player.append(f"{players['lastName']}")
-                    batting_avg.append(stats['avg'])
+                    if statChoice == 1:
+                        hitting_stat.append(f"{stats['avg']}")
+                        isInt = False
+                    elif statChoice == 2:
+                        hitting_stat.append(f"{stats['hr']}")
+                        isInt = True
                 else:
                     continue
     except:
         print("Error, failed to grab player data")
         continue
-    print(player)
-    print(batting_avg)
-    userExit = input("Would you like to exit (y/n): ")
-    if userExit == "y":
-        break
-    else:
-        player.clear()
-        batting_avg.clear()
-        continue
 
-generatePlot(player, batting_avg)
+    userExit = input("Would you like to exit (y/n): ")
+    graph = input("Would you want a chart? (y/n): ")
+    if userExit == "y" and graph == "n":
+        break
+    elif userExit == "y" and graph == "y":
+        generatePlot(player, hitting_stat, isInt)
+        break
+    elif userExit == "n" and graph == "y":
+        generatePlot(player, hitting_stat, isInt)
+        player.clear()
+        hitting_stat.clear()
+        continue
+    elif userExit == "n" and graph == "n":
+        player.clear()
+        hitting_stat.clear()
+        continue
 
 
 
--- plot.py
+++ plot.py
@@ -2,18 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def generatePlot(player, battingAverage):
-    INTbattingAverage = [float(x) for x in battingAverage]
-    fig, ax = plt.subplots()
-    colors = ['red' if player == 'Alvarez' else 'steelblue' for player in player]
-    ax.bar(player, INTbattingAverage, color=colors)
-    ax.axhline(y=0.300, color='red', linestyle='--', linewidth=1.5, label='League Average (.300)')
+def generatePlot(player, hitting_stat, isINT):
 
-    ax.set_ylabel('Batting Average')
-    ax.set_title('Batting Averages by Player')
-    ax.set_ylim(0.0, 0.400)
-    ax.set_yticks(np.arange(.0, 0.400, 0.030))# batting avg realistically caps under .400
+    try:
+        if isINT:
+            fig, ax = plt.subplots()
+            colors = ['red' if player == 'Alvarez' else 'steelblue' for player in player]
+            ax.axhline(y=20, color='red', linestyle='--', linewidth=1.5, label='League Average (.300)')
+            batting_average = [int(x) for x in hitting_stat]
+            ax.bar(player, batting_average, color=colors)
 
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.show()
+            ax.set_ylabel('Home Runs')
+            ax.set_title('Home Runs by Player')
+            ax.set_ylim(0, max(batting_average)+10)
+            ax.set_yticks(np.arange(0, max(batting_average)+10, 5))# batting avg realistically caps under .400
+
+            plt.xticks(rotation=90)
+            plt.tight_layout()
+            plt.show()
+        elif not isINT:
+            fig, ax = plt.subplots()
+            hitting_stats = [float(x) for x in hitting_stat]
+            colors = ['red' if player == 'Alvarez' else 'steelblue' for player in player]
+            ax.bar(player, hitting_stats, color=colors)
+            ax.axhline(y=0.600, color='red', linestyle='--', linewidth=1.5, label='League Average (.300)')
+
+            ax.set_ylabel('Batting Average')
+            ax.set_title('Batting Averages by Player')
+            ax.set_ylim(0.0, 0.400)
+            ax.set_yticks(np.arange(.0, 0.400, 0.030))  # batting avg realistically caps under .400
+
+            plt.xticks(rotation=90)
+            plt.tight_layout()
+            plt.show()
+    except:
+        print("Error, failed to generate chart")
