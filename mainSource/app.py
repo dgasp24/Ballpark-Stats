da089c8d17d3bdab34565623114ef516db71ad74
dgasp24 <dommygabby7@gmail.com>
2026-08-22 23:09:04 -0500

    changed which port is ran if on website deployment. and added requirements.txt

--- mainSource/app.py
+++ mainSource/app.py
@@ -1,5 +1,5 @@
 import time
-
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from mainFunctions import loadTeams, grabbingStatsforBar, grabbingStatsforScatter
 from plot import generateBarGraph, generateScatterPlot
@@ -89,4 +89,6 @@ def mlb():
 
 
 
-app.run(host='0.0.0.0', port=5000, debug = True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
